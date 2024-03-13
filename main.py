5582acb1ead1e5789972fa0f52abea0805b45caa
aL3ee27 <162388292+aL3ee27@users.noreply.github.com>
2024-03-13 01:24:46 +0000

    NEW FILE

---
+++ main.py
@@ -0,0 +1,9 @@
+# pedir dos numeros
+# a y b
+A = int(input("ingrese el numero 01:"))
+B = int(input("ingrese el numero 02:"))
+if A > B:
+    M = A
+else: 
+    M = B 
+print("El mayor es:" +str(M))
